2dc56e8db1b7b53047ed3599e37f9e83d53d76d1
Chitranjan <chitranjan.it@gmail.com>
2021-08-22 09:40:55 +0545

    desgin

---
+++ NobodysFool/assets/js/main.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($) {
+
+    function mainSlider(){
+        $('#bannerSlider').owlCarousel({
+            loop:true,
+            margin:10,
+            nav:true,
+            items:1,
+        })
+    }
+
+    // 
+    mainSlider();
+
+
+});
